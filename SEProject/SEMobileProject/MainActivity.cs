6f979a8945ecc1d8caf2e46a288802e05e618265
Raymond Ivey <Rivey724@gmail.com>
2018-10-30 22:04:38 -0700

    First Commit, Haven't Tested yet

---
+++ SEProject/SEMobileProject/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Collections.Generic;
+using Android.Content.PM;
+using Android.Provider;
+using System;
+using Android.Graphics;
+using System.IO;
+
+namespace SEMobileProject
+{
+    [Activity(Label = "SEMobileProject", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.StartMenu);
+
+            Button Login = FindViewById<Button>(Resource.Id.StartMenuLoginButton);
+
+            Login.Click += Login_Click;
+            
+        }
+        
+        private void Login_Click(object sender, EventArgs e)
+        {
+            SetContentView(Resource.Layout.MainMenu);
+        }
+       
+    }
+
+    
+}
+
